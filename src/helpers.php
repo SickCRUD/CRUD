3734d9d6cf1bd479a91e574bb17ab70220289303
Emanuele Coppola <emanuele.coppola99@gmail.com>
2017-12-29 04:41:48 +0100

    Added helpers file

---
+++ src/helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!function_exists('SickCRUD_config')) {
+    /**
+     * Little helper to get the SickCRUD config directly by passing the key
+     *
+     * @param  array|string  $key
+     * @param  mixed  $default
+     *
+     * @return mixed|\Illuminate\Config\Repository
+     */
+    function SickCRUD_config($key = null, $default = null)
+    {
+        return config('SickCRUD.' . $key, $default);
+    }
+}

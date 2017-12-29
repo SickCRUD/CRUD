0962a8b01b20709f4e47803b38c7d36354028cb4
Emanuele Coppola <emanuele.coppola99@gmail.com>
2017-12-29 18:38:01 +0100

    Added layout style

---
+++ src/config/SickCRUD/layout.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+     * --------------------------------------------------------------------------
+     * SickCRUD\CRUD Layout preferences
+     * --------------------------------------------------------------------------
+     */
+
+    'test-setting' => true,
+
+    /**
+     * Styles to include globally into the layout
+     */
+    'styles' => [
+        [
+            // this path can be a local path or eventually a CDN link
+            'path' => 'ciao/custom.css',
+            // set local true if you want to wrap the path up with the asset() function, the default value is true
+            'local' => true
+        ]
+    ],
+
+    /**
+     * Logo to show in the top-left of the navbar
+     */
+    'logo' => [
+        // TODO: finish logo config
+    ]
+
+];

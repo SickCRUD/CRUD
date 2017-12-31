7c0aac4117b7913b61442eda4beb59906cd64185
Emanuele Coppola <emanuele.coppola99@gmail.com>
2017-12-31 03:41:34 +0100

    Title setting

--- publishes/config/SickCRUD/layout.php
+++ publishes/config/SickCRUD/layout.php
@@ -8,7 +8,17 @@ return [
      * --------------------------------------------------------------------------
      */
 
-    'test-setting' => true,
+    /**
+     * Page title settings, this can be overwritten from the /resources/views/vendor/SickCRUD/layout/partials/title.blade.php view
+     */
+    'page-title' => [
+        // specify the title format, try to change it to '%title v2'
+        'title-format' => '%title',
+        // specify the title separator, 'MyAwesomeTitle - SickCRUD' where '-' is the separator
+        'title-separator' => '-',
+        // specify the suffix for the title after the separator
+        'title-suffix' => 'SickCRUD'
+    ],
 
     /**
      * Styles to include globally into the layout

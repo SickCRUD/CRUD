975d12c356ce9433cb8c484137c63875522a6d63
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-01-02 23:28:56 +0100

    Added misc translations

--- publishes/resources/lang/en/misc.php
+++ publishes/resources/lang/en/misc.php
@@ -8,6 +8,12 @@ return [
     'pages' => [
         'login' => 'Login',
         'register' => 'Register',
-    ]
+    ],
+
+    'email' => 'Email',
+    'forgot_password' => 'Forgot Password?',
+    'login' => 'Login',
+    'password' => 'Password',
+    'remember_me' => 'Remember me',
 
 ];
--- publishes/resources/lang/it/misc.php
+++ publishes/resources/lang/it/misc.php
@@ -8,6 +8,12 @@ return [
     'pages' => [
         'login' => 'Login',
         'register' => 'Registrazione',
-    ]
+    ],
+
+    'email' => 'Email',
+    'forgot_password' => 'Password dimenticata?',
+    'login' => 'Login',
+    'password' => 'Password',
+    'remember_me' => 'Ricordami',
 
 ];

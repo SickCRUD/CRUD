9eb9db26265961b0e06868967bf7051585d96ceb
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-01-06 22:12:53 +0100

    Added registration routes

--- src/routes/SickCRUD/routes.php
+++ src/routes/SickCRUD/routes.php
@@ -4,16 +4,27 @@
 Route::group(
     [
         'namespace' => 'SickCRUD\CRUD\App\Http\Controllers',
-        'middleware' => 'web'
+        'middleware' => [
+            'web',
+            'ForceHttps'
+        ]
     ],
     function () {
 
         // if is strictly trues
         if(SickCRUD_config('crud', 'setup-auth-routes') == true) {
+            // authentication
             Route::get('login', 'Auth\LoginController@show')->name('SickCRUD.auth.login');
             Route::post('login', 'Auth\LoginController@login');
             Route::get('logout', 'Auth\LoginController@logout')->name('SickCRUD.auth.logout');
             Route::post('logout', 'Auth\LoginController@logout');
+
+            // registration
+            if(SickCRUD_config('crud', 'setup-register-routes') == true) {
+                Route::get('register', 'Auth\RegisterController@show')->name('SickCRUD.auth.register');
+                Route::post('register', 'Auth\RegisterController@login');
+            }
+
         }
 
     }

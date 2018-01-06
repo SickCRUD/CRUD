165329785c88e57b5bddea42e4c1a7b88cd99c93
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-01-06 22:12:30 +0100

    Added ForceHttps middleware

---
+++ src/app/Http/Middleware/ForceHttps.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SickCRUD\CRUD\App\Http\Middleware;
+
+use Closure;
+
+class ForceHttps
+{
+    /**
+     * Force non-local requests to be HTTPS
+     * From: https://gist.github.com/barryvdh/2578593e89d252737279f40b82be5b61
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param  string|null $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (SickCRUD_config('crud', 'force-https') == true && !$request->isSecure()) {
+            return redirect()->secure($request->getRequestUri());
+        }
+        return $next($request);
+    }
+}

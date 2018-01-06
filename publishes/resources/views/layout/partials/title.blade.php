7454ef8829c168fdb3adcb64b8bb9e33313caf1b
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-01-06 22:14:29 +0100

    Fixed error if title empty

--- publishes/resources/views/layout/partials/title.blade.php
+++ publishes/resources/views/layout/partials/title.blade.php
@@ -1,3 +1,3 @@
 <title>
-    {{ SickCRUD_title_format($pageTitle) }} {{ SickCRUD_config('layout', 'page-title.title-separator') }} {{ SickCRUD_config('layout', 'page-title.title-suffix') }}
+    {{ SickCRUD_title_format($pageTitle ?? '') }} {{ SickCRUD_config('layout', 'page-title.title-separator') }} {{ SickCRUD_config('layout', 'page-title.title-suffix') }}
 </title>

5594155437f7b7c5c39972cc850972d8c0825c73
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-01-06 22:11:12 +0100

    Added useful settings

--- publishes/config/SickCRUD/crud.php
+++ publishes/config/SickCRUD/crud.php
@@ -9,8 +9,24 @@ return [
      */
 
     /**
+     * Should SickCRUD change the default schema sting lenght to 191?
+     */
+    'change-schema-string-length' => true,
+
+    /**
      * Should SickCRUD setup the auth routes or not?
      */
     'setup-auth-routes' => true,
+
+    /**
+     * Should SickCRUD setup the register routes or not?
+     */
+    'setup-register-routes' => true,
+
+    /**
+     * Should SickCRUD redirect HTTP requests to HTTPS?
+     */
+    'force-https' => false,
+
 
 ];

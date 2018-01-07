98cf30921dedda90e952fdecaede612b142247e7
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-01-07 02:58:05 +0100

    Changed routes code

--- src/routes/SickCRUD/routes.php
+++ src/routes/SickCRUD/routes.php
@@ -14,17 +14,26 @@ Route::group(
         // if is strictly trues
         if(SickCRUD_config('crud', 'setup-auth-routes') == true) {
             // authentication
-            Route::get('login', 'Auth\LoginController@show')->name('SickCRUD.auth.login');
+            Route::get('login', [
+                'as' => 'SickCRUD.auth.login',
+                'uses' => 'Auth\LoginController@show'
+            ]);
             Route::post('login', 'Auth\LoginController@login');
-            Route::get('logout', 'Auth\LoginController@logout')->name('SickCRUD.auth.logout');
+            Route::get('logout', [
+                'as' => 'SickCRUD.auth.logout',
+                'uses' => 'Auth\LoginController@logout'
+            ]);
             Route::post('logout', 'Auth\LoginController@logout');
 
-            // registration
-            if(SickCRUD_config('crud', 'setup-register-routes') == true) {
-                Route::get('register', 'Auth\RegisterController@show')->name('SickCRUD.auth.register');
-                Route::post('register', 'Auth\RegisterController@login');
-            }
+        }
 
+        // registration
+        if(SickCRUD_config('crud', 'setup-register-routes') == true) {
+            Route::get('register', [
+                'as' => 'SickCRUD.auth.register',
+                'uses' => 'Auth\RegisterController@show'
+            ]);
+            Route::post('register', 'Auth\RegisterController@login');
         }
 
     }

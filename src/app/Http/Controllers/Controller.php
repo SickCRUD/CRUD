a013a707518ff66b4525c6c7ffb4c67515296e92
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-01-11 07:20:41 +0100

    Added view data trait in crud controller

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -6,8 +6,12 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+// SickCRUD
+use SickCRUD\CRUD\Core\Traits\ViewData;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests,
+        // SickCRUD traits
+        ViewData;
 }

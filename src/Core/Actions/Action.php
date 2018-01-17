70137b48cb8671b8238766d8d56c6206b65fa254
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-01-17 22:27:24 +0100

    Added action base controller

--- src/Core/Actions/Action.php
+++ src/Core/Actions/Action.php
@@ -2,25 +2,73 @@
 
 namespace SickCRUD\CRUD\Core\Actions;
 
-
 class Action
 {
-    protected $actionName = '';
-    protected $actionRequests = [];
+    /**
+     * Define the action name.
+     *
+     * @var string
+     */
+    protected static $actionName = '';
 
-    public function __construct()
-    {
-        foreach ($this->actionRequests as $actionRequest) {
-            //
-        }
-    }
+    /**
+     * Define if the current actions need to take the id in the route.
+     *
+     * @var bool
+     */
+    public static $requireIdParam = false;
 
-    public function getActionName()
+    /**
+     * Get the current action name.
+     *
+     * @return string
+     */
+    public static function getActionName()
     {
         // get the class that is going to extend
-        $parentClassBasename = basename(get_parent_class($this));
+        $parentClassBasename = basename(get_parent_class(static::class));
         // remove the class suffix if contained to get the action name
-        return $this->actionName ?$this->actionName : strtolower(preg_replace('/'. $parentClassBasename .'$/', '', class_basename($this)));
+        return static::$actionName ?static::$actionName : strtolower(preg_replace('/'. $parentClassBasename .'$/', '', class_basename(static::class)));
+    }
+
+    /**
+     * Returns the routes related with the actions.
+     *
+     * @return array
+     */
+    public static function getActionRoutes()
+    {
+        // regex to match for the functions
+        $sickActionRoutePattern = '/^action(Get|Patch|Post|Put)(.*)/';
+
+        // return an array with the filtered functions
+        $sickActionRouteFunctions = array_values(preg_grep($sickActionRoutePattern, get_class_methods(new static())));
+
+        // action routes array
+        $sickActionRoutes = [];
+
+        foreach ($sickActionRouteFunctions as $sickActionRouteFunction) {
+
+            // regex to get the values
+            preg_match_all($sickActionRoutePattern, $sickActionRouteFunction, $sickActionRouteMatch);
+
+            // discover the HTTP verb
+            $routeMethod = strtolower(reset($sickActionRouteMatch[1]));
+
+            // get the route name
+            $routeName = strtolower(reset($sickActionRouteMatch[2]));
+
+            // store the routes
+            $sickActionRoutes[] = [
+                'function' => $sickActionRouteFunction,
+                'name' => $routeName,
+                'method' => $routeMethod,
+            ];
+
+        }
+
+        return $sickActionRoutes;
+
     }
 
 }

afa340423edd5ac1af79f69fa0ba50c62b931fca
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-01-23 01:27:27 +0100

    Fixed responsive navbar

--- publishes/resources/views/layout/partials/navbar.blade.php
+++ publishes/resources/views/layout/partials/navbar.blade.php
@@ -16,41 +16,37 @@
     {{-- TODO: add a setting to customize the scroll of the navbar (fixed or not) --}}
     <nav class="navbar navbar-expand-md navbar-dark">
 
-        <div class="collapse navbar-collapse">
+        <ul class="navbar-nav d-flex flex-row ml-auto">
 
-            <ul class="navbar-nav ml-auto">
+            @if (SickCRUD_config('crud', 'setup-auth-routes') == true)
 
-                @if (SickCRUD_config('crud', 'setup-auth-routes') == true)
+                @if (Auth::guest())
 
-                    @if (Auth::guest())
+                    <li class="nav-item {{ URL::current() == URL::route('SickCRUD.auth.login') ? 'active' : '' }}">
+                        <a class="nav-link p-2" href="{{ URL::route('SickCRUD.auth.login') }}">{{ Lang::get('SickCRUD::misc.pages.login') }} <span class="sr-only">({{ Lang::get('SickCRUD::misc.current') }})</span></a>
+                    </li>
 
-                        <li class="nav-item {{ URL::current() == URL::route('SickCRUD.auth.login') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ URL::route('SickCRUD.auth.login') }}">{{ Lang::get('SickCRUD::misc.pages.login') }} <span class="sr-only">({{ Lang::get('SickCRUD::misc.current') }})</span></a>
-                        </li>
+                    @if (SickCRUD_config('crud', 'setup-register-routes'))
 
-                        @if (SickCRUD_config('crud', 'setup-register-routes'))
-
-                            <li class="nav-item {{ URL::current() == URL::route('SickCRUD.auth.register') ? 'active' : '' }}">
-                                <a class="nav-link" href="{{ URL::route('SickCRUD.auth.register') }}">{{ Lang::get('SickCRUD::misc.pages.register') }} <span class="sr-only">({{ Lang::get('SickCRUD::misc.current') }})</span></a>
-                            </li>
-
-                        @endif
-
-                    @else
-
-                        <li class="nav-item">
-                            <a href="{{ URL::route('SickCRUD.auth.logout') }}" class="nav-link">
-                                <i class="fa fa-btn fa-sign-out"></i> {{ trans('SickCRUD::misc.') }}
-                            </a>
+                        <li class="nav-item {{ URL::current() == URL::route('SickCRUD.auth.register') ? 'active' : '' }}">
+                            <a class="nav-link p-2" href="{{ URL::route('SickCRUD.auth.register') }}">{{ Lang::get('SickCRUD::misc.pages.register') }} <span class="sr-only">({{ Lang::get('SickCRUD::misc.current') }})</span></a>
                         </li>
 
                     @endif
 
+                @else
+
+                    <li class="nav-item">
+                        <a href="{{ URL::route('SickCRUD.auth.logout') }}" class="nav-link">
+                            <i class="fa fa-btn fa-sign-out"></i> {{ trans('SickCRUD::misc.') }}
+                        </a>
+                    </li>
+
                 @endif
 
-            </ul>
+            @endif
 
-        </div>
+        </ul>
 
     </nav>
 

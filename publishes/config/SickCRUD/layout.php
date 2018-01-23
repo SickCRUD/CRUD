c3a4ac0d292ed1db58169a232931036bb8748add
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-01-23 02:38:57 +0100

    Changed layout settings

--- publishes/config/SickCRUD/layout.php
+++ publishes/config/SickCRUD/layout.php
@@ -21,38 +21,6 @@ return [
     ],
 
     /**
-     * Styles to include globally into the layout
-     */
-    'styles' => [
-        [
-            // this path can be a local path or eventually a CDN link
-            'path' => 'ciao/custom.css',
-            // set local true if you want to wrap the path up with the asset() function, the default value is true
-            'local' => true
-        ],
-        [
-            'path' => 'vendor/bootstrap/css/bootstrap.min.css',
-            'local' => true
-        ],
-        [
-            'path' => 'css/sick-crud.min.css',
-            'local' => true
-        ]
-    ],
-
-    /**
-     * Styles to include globally into the layout
-     */
-    'scripts' => [
-        [
-            // this path can be a local path or eventually a CDN link
-            'path' => 'ciao/custom.js',
-            // set local true if you want to wrap the path up with the asset() function, the default value is true
-            'local' => true
-        ]
-    ],
-
-    /**
      * Navbar settings
      */
     'navbar' => [

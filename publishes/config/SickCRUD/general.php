29035d2ba980282f022d80352fd64de88ccc615c
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-02-06 02:17:56 +0100

    Added general behaves config

---
+++ publishes/config/SickCRUD/general.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /*
+    * --------------------------------------------------------------------------
+    * SickCRUD\CRUD General behaves preferences
+    * --------------------------------------------------------------------------
+    */
+
+    // this is shown on error pages, leave it empty or false to hide it (it should be an email)
+    'technical-contact' => ''
+
+];

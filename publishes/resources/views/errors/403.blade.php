579caed7bd3e0ab2bac5efb3480b2237e9563adf
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-02-25 22:13:49 +0100

    Fixed indentation

--- publishes/resources/views/errors/403.blade.php
+++ publishes/resources/views/errors/403.blade.php
@@ -2,7 +2,8 @@
 <html lang="{{ App::getLocale() }}">
 <head>
     <!-- Simple HttpErrorPages | MIT License | https://github.com/AndiDittrich/HttpErrorPages -->
-    <meta charset="utf-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>{{ rtrim(Lang::get('SickCRUD::misc.errors.403.title'), '.') }} | 403 - {{ rtrim(Lang::get('SickCRUD::misc.errors.403.heading'), '.') }}</title>
     <link rel="stylesheet" href="{{ SickCRUD_asset('vendor/bootstrap/css/bootstrap.min.css', true) }}">

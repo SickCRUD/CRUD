d86341f60648ca67bd2edc0de9f8cc6f5ae38607
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-02-25 22:51:24 +0100

    Proper tests

---
+++ tests/Unit/Actions/ActionPropertiesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SickCRUD\CRUD\Tests\Unit\Actions;
+
+
+class ActionPropertiesTest extends BaseActionTest
+{
+    /**
+     * Check if the prefix is needed (should match our TestAction class).
+     */
+    public function testIfPrefixIsNeeded()
+    {
+        $this->assertSame(true, $this->action->actionRoutePrefix);
+    }
+
+    /**
+     * Check if the prefix is needed (should match our TestAction class).
+     */
+    public function testIfIdParamIsRequired()
+    {
+        $this->assertSame(true, $this->action->actionRequireIdParam);
+    }
+}
---
+++ tests/Unit/Actions/ActionRoutesTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace SickCRUD\CRUD\Tests\Unit\Actions;
+
+
+class ActionRoutesTest extends BaseActionTest
+{
+    /**
+     * Test if the routes are parsed correctly.
+     */
+    public function testActionRoutes()
+    {
+        // actual extracted array
+        $actionRoutesExpected = [
+            [
+                'function' => 'actionGet',
+                'method' => 'get',
+                'name' => false
+            ],
+            [
+                'function' => 'actionPatch',
+                'method' => 'patch',
+                'name' => false
+            ],
+            [
+                'function' => 'actionPost',
+                'method' => 'post',
+                'name' => false
+            ],
+            [
+                'function' => 'actionPut',
+                'method' => 'put',
+                'name' => false
+            ],
+            [
+                'function' => 'actionGetTest',
+                'method' => 'get',
+                'name' => 'test'
+            ],
+            [
+                'function' => 'actionPatchTest',
+                'method' => 'patch',
+                'name' => 'test'
+            ],
+            [
+                'function' => 'actionPostTest',
+                'method' => 'post',
+                'name' => 'test'
+            ],
+            [
+                'function' => 'actionPutTest',
+                'method' => 'put',
+                'name' => 'test'
+            ]
+        ];
+
+        // array of extracted action routes
+        $actionRoutes = $this->action->getRoutes();
+
+        // check if the count is the same
+        $this->assertCount(count($actionRoutesExpected), $actionRoutes, 'The action returned routes count is different compared to the expected one.');
+
+        // cycle them for the assertion
+        foreach ($actionRoutes as $key => $actionRoute) {
+
+            $this->assertContains($actionRoute, $actionRoutesExpected, 'Array not matching on index ' . $key . '.');
+
+        }
+
+    }
+
+}

15e4a0e59668b45d74f4bdbceb91acb539db6615
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-02-28 21:31:49 +0100

    Fixed case-sensitive folder

--- tests/BrowserTestCase.php
+++ tests/BrowserTestCase.php
@@ -38,7 +38,7 @@ abstract class BrowserTestCase extends TestCase
         parent::setUp();
 
         $this->prepareSqLite();
-        $this->withFactories(__DIR__ . '/app/factories');
+        $this->withFactories(__DIR__ . '/app/Factories');
         $this->loadLaravelMigrations(config('database.default'));
 
     }

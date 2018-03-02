dc5e61f96e9df6d5585cd6068dfca3450c12259d
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-03-03 00:12:39 +0100

    Updated tos view to support errors

--- publishes/resources/views/pages/auth/partials/tos.blade.php
+++ publishes/resources/views/pages/auth/partials/tos.blade.php
@@ -1,9 +1,19 @@
 <div class="row no-gutters align-items-center justify-content-between">
 
-    <div class="form-check mx-auto">
-        <label class="form-check-label">
-            <input type="checkbox" name="tos" class="form-check-input"> {{ Lang::get('SickCRUD::auth.tos-read-and-accept') }} <a href="#">{{ Lang::get('SickCRUD::auth.tos-terms-and-conditions') }}</a>
-        </label>
+    <div class="form-group mx-auto mb-0">
+
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" name="tos" id="tos">
+            <label class="form-check-label" for="tos">
+                {{ Lang::get('SickCRUD::auth.tos-read-and-accept') }} <a href="#">{{ Lang::get('SickCRUD::auth.tos-terms-and-conditions') }}</a>
+            </label>
+            @if ($errors->has('tos'))
+                <div class="invalid-feedback mt-0">
+                    {{ $errors->first('tos') }}
+                </div>
+            @endif
+        </div>
+
     </div>
 
 </div>

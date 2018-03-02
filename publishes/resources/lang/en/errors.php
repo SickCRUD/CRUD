d84930dd33f6f73376b9a3919e0bdc1f84ec571a
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-03-02 23:09:11 +0100

    Updated auth errors translations

--- publishes/resources/lang/en/errors.php
+++ publishes/resources/lang/en/errors.php
@@ -5,12 +5,14 @@
  */
 return [
 
+    '403' => [
+        'description' => 'The requested resource requires an authentication.',
+        'heading' => 'Access Denied.',
+        'title' => 'We\'ve got some trouble.',
+    ],
+
     'error' => 'Error',
     'technical_contact' => 'Technical contact',
-    '403' => [
-        'title' => 'We\'ve got some trouble.',
-        'heading' => 'Access Denied.',
-        'description' => 'The requested resource requires an authentication.',
-    ],
+
 
 ];
---
+++ publishes/resources/lang/it/errors.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * SickCRUD EN Errors translations.
+ */
+return [
+
+    '403' => [
+        'description' => 'La risorsa richiesta richiede autenticazione.',
+        'heading' => 'Accesso negato.',
+        'title' => 'Abbiamo avuto dei problemi.',
+    ],
+
+    'error' => 'Errore',
+    'technical_contact' => 'Contatto tecnico',
+
+];

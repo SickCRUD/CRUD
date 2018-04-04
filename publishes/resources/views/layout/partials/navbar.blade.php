13e0dc2998c0cd9d80f59f3d675c1fec1e201534
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-04-04 22:11:40 +0200

    Changed navbar to AdminLTE

--- publishes/resources/views/layout/partials/navbar.blade.php
+++ publishes/resources/views/layout/partials/navbar.blade.php
@@ -1,41 +1,40 @@
-<div class="navbar-wrapper {{ SickCRUD_config('layout', 'navbar.navbar-fixed') == true ? 'fixed-top' : '' }}">
+<!-- Logo -->
+<a href="{{ SickCRUD_url('/') }}" class="logo {{ SickCRUD_config('layout', 'navbar.navbar-fixed') == true ? 'position-fixed' : '' }}">
+    <span class="logo-mini">
+        {!! SickCRUD_config('layout', 'navbar.logo.text.logo-mini') !!}
+    </span>
+    <span class="logo-lg">
+        {!! SickCRUD_config('layout', 'navbar.logo.text.logo-large') !!}
+    </span>
+</a>
 
-    <a href="{{ SickCRUD_url('/') }}" class="logo">
-
-        {{-- TODO: accept logo IMG --}}
-        <span class="logo-mini">
-            {!! SickCRUD_config('layout', 'navbar.logo.text.logo-mini') !!}
-        </span>
-
-        <span class="logo-large">
-            {!! SickCRUD_config('layout', 'navbar.logo.text.logo-large') !!}
-        </span>
-
+<nav class="navbar {{ SickCRUD_config('layout', 'navbar.navbar-fixed') == true ? 'navbar-fixed-top' : 'navbar-static-top' }}" role="navigation">
+    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
     </a>
 
-    <nav class="navbar navbar-expand-md navbar-dark">
-
-        <ul class="navbar-nav d-flex flex-row ml-auto">
+    <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
 
             @if (SickCRUD_config('crud', 'setup-auth-routes') === true)
 
                 @if (Auth::guest())
 
-                    <li class="nav-item {{ URL::current() == URL::route('SickCRUD.auth.login') ? 'active' : '' }}">
-                        <a class="nav-link p-2" href="{{ URL::route('SickCRUD.auth.login') }}">{{ Lang::get('SickCRUD::auth.pages.login') }} <span class="sr-only">({{ Lang::get('SickCRUD::misc.current') }})</span></a>
+                    <li class="{{ URL::current() == URL::route('SickCRUD.auth.login') ? 'active' : '' }}">
+                        <a href="{{ URL::route('SickCRUD.auth.login') }}">{{ Lang::get('SickCRUD::auth.pages.login') }} <span class="sr-only">({{ Lang::get('SickCRUD::misc.current') }})</span></a>
                     </li>
 
                     @if (SickCRUD_config('crud', 'setup-register-routes'))
 
-                        <li class="nav-item {{ URL::current() == URL::route('SickCRUD.auth.register') ? 'active' : '' }}">
-                            <a class="nav-link p-2" href="{{ URL::route('SickCRUD.auth.register') }}">{{ Lang::get('SickCRUD::auth.pages.register') }} <span class="sr-only">({{ Lang::get('SickCRUD::misc.current') }})</span></a>
+                        <li class="{{ URL::current() == URL::route('SickCRUD.auth.register') ? 'active' : '' }}">
+                            <a href="{{ URL::route('SickCRUD.auth.register') }}">{{ Lang::get('SickCRUD::auth.pages.register') }} <span class="sr-only">({{ Lang::get('SickCRUD::misc.current') }})</span></a>
                         </li>
 
                     @endif
 
                 @else
 
-                    <li class="nav-item">
+                    <li>
                         <a href="{{ URL::route('SickCRUD.auth.logout') }}" class="nav-link">
                             <i class="fa fa-btn fa-sign-out"></i> {{ trans('SickCRUD::auth.actions.logout') }}
                         </a>
@@ -44,11 +43,7 @@
                 @endif
 
             @endif
-
         </ul>
-
-    </nav>
-
-</div>
-
+    </div>
+</nav>
 

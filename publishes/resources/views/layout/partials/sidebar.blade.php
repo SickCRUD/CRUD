d13dafeb2630c3e99dba053a5256fcb86d95a16f
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-04-05 01:02:42 +0200

    Added sidebar sample from demo lte

---
+++ publishes/resources/views/layout/partials/sidebar.blade.php
@@ -0,0 +1,29 @@
+<section class="sidebar">
+
+    <div class="user-panel">
+        <div class="pull-left image">
+            <img src="http://via.placeholder.com/200x200" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+            <p>Alexander Pierce</p>
+            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+    </div>
+
+    <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
+        <li class="treeview active">
+            <a href="#">
+                <i class="fas fa-tachometer-alt"></i>&nbsp;&nbsp;<span>Dashboard</span>
+                <span class="pull-right-container">
+          <i class="fa fa-angle-left pull-right"></i>
+        </span>
+            </a>
+            <ul class="treeview-menu">
+                <li><a href="../../index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
+                <li><a href="../../index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+            </ul>
+        </li>
+    </ul>
+
+</section>

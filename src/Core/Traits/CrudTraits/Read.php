02e563415e85aa753027bbcdb0a664612719365d
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-04-23 01:37:44 +0200

    Updated query var name

--- src/Core/Traits/CrudTraits/Read.php
+++ src/Core/Traits/CrudTraits/Read.php
@@ -5,6 +5,13 @@ namespace SickCRUD\CRUD\Core\Traits\CrudTraits;
 trait Read
 {
     /**
+     * It contains the current query to be executed.
+     *
+     * @var mixed
+     */
+    public $readQuery = false;
+
+    /**
      * Find and retrieve an entry in the database or fail.
      *
      * @param int|string The id of the row to fetch.
@@ -28,10 +35,9 @@ trait Read
      */
     public function getEntries()
     {
-
         // if the query instance isset and valid, then return it
-        if ($this->query && get_class($this->query) == \Illuminate\Database\Eloquent\Builder::class) {
-            return $this->query->get();
+        if ($this->readQuery && get_class($this->readQuery) == \Illuminate\Database\Eloquent\Builder::class) {
+            return $this->readQuery->get();
         }
 
         return $this->getModel()->select('*')->get();
